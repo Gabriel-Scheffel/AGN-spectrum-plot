f5ac3dac4cda2956afffc3590f062e9ef555203e
Gabriel Scheffel <73002431+Gabriel-Scheffel@users.noreply.github.com>
2022-11-14 19:56:51 -0300

    add notes

--- plot-espectro.py
+++ plot-espectro.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from lines import lines
 '''
+rascunhos e lembretes:
 matriz[linha][coluna]
 plt.plo(x, y)
 
@@ -9,7 +10,7 @@ NGC 5548: z = 0.01627
 '''
 z = 0.01627  # redshift
 with fits.open('spec-2127-53859-0085.fits') as hdul:
-    # hdul.info()
+    # hdul.info() # informações sobre o documento.fit
     coadd = hdul[1].data
 
 flux = []
@@ -22,4 +23,11 @@ for i in range(len(coadd)):
 fig = plt.xlim(3.5e3, 9.5e3)
 plt.plot(wlen, flux, 'black')
 lines.plotlines(3.5e3, 9.5e3, 5e3)
+'''
+A função plotlines recebe três valores:
+- um limite mínimo e um máximo para o x;
+    linhas fora desses valores não são plotadas no gráfico.
+- e outro valor máximo para o y;
+    define a altura máxima das legendas.
+'''
 plt.show()
